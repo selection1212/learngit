aa2e682b0906a70029d8ae02d1b729ab709e3501
dai <dai@qq.com>
2017-11-30 15:44:45 +0800

    b

---
+++ lodash/daizy1212-lodash.js
@@ -0,0 +1,10 @@
+var daizy1212 = {
+    chunk: function (array,length=1){
+        var result = [];
+        for(i = 0; i < arry.length; i +=length){
+            result.push(arry.slice(i,i+length));
+        }
+         return result;
+    },
+}
+
