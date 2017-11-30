25e2c7ed6fd7466bb7f5ca70f8eca68797d60a8c
dai <dai@qq.com>
2017-11-30 15:47:36 +0800

    3

--- lodash/daizy1212-lodash.js
+++ lodash/daizy1212-lodash.js
@@ -1,8 +1,8 @@
 var daizy1212 = {
     chunk: function (array,length=1){
         var result = [];
-        for(i = 0; i < arry.length; i +=length){
-            result.push(arry.slice(i,i+length));
+        for(i = 0; i < array.length; i +=length){
+            result.push(array.slice(i,i+length));
         }
          return result;
     },
